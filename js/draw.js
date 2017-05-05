1774b381f977a3d578b0f0f40d9fbd815f8072cc
Tianyi He <tianyi.he@morningstar.com>
2017-05-05 01:31:08 -0500

    Tianyi - add reponsive width

--- js/draw.js
+++ js/draw.js
@@ -1,6 +1,6 @@
-var width = 800,
+var width = document.getElementById("svg-container").clientWidth,
     height = 200,
-    padding = 15;
+    padding = 10;
 
 var xScale = d3.scaleLinear()
     .domain([6, 9.5]) //min rating to highest
@@ -11,19 +11,16 @@ var rScale = d3.scaleLinear()
     .range([0, (height-padding)/2]);
 
 var xAxis = d3.axisBottom(xScale);
-
-var showMovieInfo = (movie) => {
+var showMovieInfo = (movie) => {    
     var table = $("#movie-info table").get();
     $(table).find(".title-row td").text(movie.title);
-//    $(table).find(".actor-row td").text(movie.actor);
     $(table).find(".year-row td").text(movie.year);
     $(table).find(".gross-row td").text("$" + movie.gross);
     $(table).find(".rating-row td").text(movie.rating);
-//    console.log(title);
 }
 
 for (var actor in data) {
-    var svg = d3.select("section.movies")
+    var svg = d3.select("#svg-container")
                 .append("svg")
                 .attr("class", "svg-movie")
                 .attr("width", width)
@@ -48,8 +45,10 @@ for (var actor in data) {
     .attr("fill", "pink")
     .on("mouseover", (d) => {
         showMovieInfo(d);
+//        d3.select(this)
+//            .attr("stroke", "red");
     });
-
+   
     svg.append("g")
         .attr("class", "svg-axis")
         .attr("transform", "translate(0," + height/2 + ")")
